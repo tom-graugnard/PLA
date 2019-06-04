0f4e55d415a49168599e118d4e54d765a6369bda
Morgan Crociati <morgan.crociati@outlook.com>
2019-06-04 15:53:28 +0200

    Taille terrain correct

--- game.shellda/src/game/shellda/GameMain.java
+++ game.shellda/src/game/shellda/GameMain.java
@@ -15,20 +15,7 @@ public class GameMain {
 
 		Dimension d = new Dimension(Options.WIDTH, Options.HEIGHT);
 		new GameUI(model, view, controller, d);
-
-		// notice that the main thread will exit here,
-		// but not your program... hence the hooking
-		// of the window events to System.exit(0) when
-		// the window is closed. See class WindowListener.
-
-		/*
-		 * *** WARNING *** WARNING *** WARNING *** WARNING *** If you do something here,
-		 * on this "main" thread, you will have parallelism and thus race conditions.
-		 * 
-		 * ONLY FOR ADVANCED DEVELOPERS
-		 * 
-		 * *** WARNING *** WARNING *** WARNING *** WARNING ***
-		 */
+		
 		return;
 	}
 }
