2c486e9190fd830c87a63d378bef5c4beac75e09
Tomascft <tom.graugnard@gmail.com>
2019-06-12 09:31:14 +0200

    Decompression

--- game.shellda/src/game/shellda/Archive.java
+++ game.shellda/src/game/shellda/Archive.java
@@ -6,23 +6,27 @@ import java.util.LinkedList;
 
 import interpreter.IKind;
 
-public class Archive extends Element{
+public class Archive extends Element {
 
 	String m_name;
-	
+
 	LinkedList<Element> m_contenu;
- 	
+
 	public Archive(Noeud courant, Model model, int x, int y, String name) {
 		super(courant, model, x, y);
 		m_name = name;
 		m_kind = new IKind("G");
 	}
 
+	public Element decompression() {
+		return new Fichier(m_courant,m_model,m_x,m_y,m_model.m_generator.generate());
+	}
+
 	public void paint(Graphics g) {
 		g.drawImage(m_model.m_archiveSprite, m_x * 48 + 8, m_y * 48 + 10, 32, 32, null);
-		
+
 		g.setFont(m_model.m_font);
 		FontMetrics f = g.getFontMetrics();
-		g.drawString(m_name, m_x * 48 + (48 - f.stringWidth(m_name))/2, m_y * 48 + 42 + (16 / 2));
+		g.drawString(m_name, m_x * 48 + (48 - f.stringWidth(m_name)) / 2, m_y * 48 + 42 + (16 / 2));
 	}
 }
